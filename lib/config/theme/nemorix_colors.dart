3e5638e31dabd773578eb3b0927707211ebc2cce
Miguel Fagundez <59711011+miguelfagundez@users.noreply.github.com>
2025-03-31 12:00:00 -0400

    Setup: Initial colors and theme data

---
+++ lib/config/theme/nemorix_colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class NemorixColors {
+  static const Color mainBlack = Color(0xFF03110A);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkModeBackground = Color(0xFF04160D);
+  static const Color lightModeBackground = Color(0xFFFFFFFF);
+  static const Color primaryColor = Color(0xFFE7C200);
+  static const Color secondaryColor = Color(0XFFF6F7F8);
+  static const Color red = Color(0xFFF65556);
+  static const Color greyLevel1 = Color(0xFF272727);
+  static const Color greyLevel2 = Color(0xFF5E5E5E);
+  static const Color greyLevel3 = Color(0xFF949494);
+  static const Color greyLevel4 = Color(0xFFCCCCCC);
+  static const Color greyLevel5 = Color(0xFFEEEEEE);
+  static const Color greyLevel6 = Color(0xFFF8F8F8);
+  static const Color green = Color(0xFF09ff04);
+}
--- lib/config/theme/nemorix_theme.dart
+++ lib/config/theme/nemorix_theme.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:nemorixpay/config/theme/nemorix_colors.dart';
 
-final ThemeData darkTheme = ThemeData.dark();
-final ThemeData lightTheme = ThemeData.light();
+class NemorixTheme {
+  static final ThemeData darkThemeMode = ThemeData.dark(
+    useMaterial3: true,
+  ).copyWith(
+    scaffoldBackgroundColor: NemorixColors.darkModeBackground,
+    appBarTheme: const AppBarTheme(backgroundColor: NemorixColors.greyLevel1),
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: NemorixColors.primaryColor,
+    ),
+  );
+  static final ThemeData lightThemeMode = ThemeData.light(
+    useMaterial3: true,
+  ).copyWith(
+    scaffoldBackgroundColor: NemorixColors.lightModeBackground,
+    appBarTheme: const AppBarTheme(backgroundColor: NemorixColors.greyLevel6),
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: NemorixColors.primaryColor,
+    ),
+  );
+}
